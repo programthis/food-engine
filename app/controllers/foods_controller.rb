bfed3721c44f1f0d34dfa682afa6f4f28183fa69
programthis <kylebachan@gmail.com>
2020-04-08 18:27:35 -0400

    Added navbar, jquery and more

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,18 @@
+class FoodsController < ApplicationController
+	def new
+		@food = Food.new
+	end
+
+	def create
+		@food = Food.new(food_params)
+		if @food.save
+			redirect_to root_path
+		else 
+			render json: { error: "Error creating food." }
+		end
+	end
+
+	def food_params
+		params.require(:food).permit(:name)
+	end
+end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,10 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+document.addEventListener("turbolinks:load", () => {
+    $(".hamburger").click(function() {
+        $(this).toggleClass("is-active");
+        $(".navbarMobileLinks").slideToggle("fast");
+    });
+});
