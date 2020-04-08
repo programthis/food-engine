6c089f019c7779357d0a33d6916792c6d1b26d24
programthis <kylebachan@gmail.com>
2020-04-08 17:50:08 -0400

    Added ingredients controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
     devise :database_authenticatable, :registerable,
            :recoverable, :rememberable, :validatable, :confirmable
     validates_uniqueness_of :email
+
+    has_many :ingredients
 end
