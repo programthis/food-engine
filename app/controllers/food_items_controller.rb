09c24ea15591a7749ac8af89dc815a72fe394bc0
programthis <kylebachan@gmail.com>
2020-04-09 12:20:41 -0400

    Users can now add items to their inventory

--- app/controllers/food_items_controller.rb
+++ app/controllers/food_items_controller.rb
@@ -4,4 +4,14 @@ class FoodItemsController < ApplicationController
 		@food_item = FoodItem.new
 		@foods = Food.all
 	end
+
+	def create
+		food = Food.find(params[:food_item][:id])
+		food_item = food.food_items.create(user: current_user)
+		redirect_to root_path
+	end
+
+	def food_item_params
+		params.require(:food_item).permit(:name, :category, :expirable)
+	end
 end
