a3e035b4d2ed64961b0295f5c04b1619f1073a33
programthis <kylebachan@gmail.com>
2020-04-10 14:27:32 -0400

    Added recipes routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
     devise_for :users
-    devise_for :foods
-
     authenticate :user do
         root to: "food_items#index"
         resources :foods
     	resources :food_items
-        # devise_scope :user do
-        #     get 'users', to: 'devise/registrations#new'
-        #     get 'users/password', to: 'devise/password#new'
-        # end
+        resources :recipes
     end
 end
